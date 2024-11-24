f665d91596b10f2ea56a0d5168ac00a7bd7f0343
sowgand <ramezanzadesogand@gmail.com>
2024-11-24 13:54:24 +0330

    Create todolist.js

---
+++ todolist.js
@@ -0,0 +1,10 @@
+// making a array for puting my task in
+
+let ToDoList = [];
+
+//function to add my task by pushin them inside the stack
+
+function addTask(task){
+    ToDoList.push(task);
+    
+}
